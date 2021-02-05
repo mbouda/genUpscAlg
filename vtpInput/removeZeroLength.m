12ca3f556056d5c34915c0c690b8ad810f761516
Martin <martin.bouda@ibot.cas.cz>
2021-02-05 23:33:51 +0100

    fixed bug in removal of zeroLengths causing mislinked parents

--- vtpInput/removeZeroLength.m
+++ vtpInput/removeZeroLength.m
@@ -1,23 +1,23 @@
 function plant=removeZeroLength(plant)
 
     isZero=plant.L==0;
-    J=find(isZero)';
+    J=fliplr(find(isZero)');
     for j=J
         dtrs=plant.parents==j;
         par=plant.parents(j);
         plant.parents(dtrs)=par;
         plant.parents(plant.parents>j)=plant.parents(plant.parents>j)-1;
+        
+        plant.cx(j,:)=[];
+        plant.cy(j,:)=[];
+        plant.cz(j,:)=[];
+    
+        plant.parents(j)=[];
+        plant.L(j)=[];
+        plant.M(j)=[];
+        plant.AX(j)=[];
+        plant.R(j)=[];        
     end
-    
-    plant.cx(J,:)=[];
-    plant.cy(J,:)=[];
-    plant.cz(J,:)=[];
-    
-    plant.parents(J)=[];
-    plant.L(J)=[];
-    plant.M(J)=[];
-    plant.AX(J)=[];
-    plant.R(J)=[];
     
     plant.nL=plant.nL-sum(isZero);
 end
